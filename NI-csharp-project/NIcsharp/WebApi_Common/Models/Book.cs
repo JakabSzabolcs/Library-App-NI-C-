c0d6787bb937cab183afab319985be646cef2db7
szabesz <jakab.szabolcs226@gmail.com>
2022-11-26 11:51:11 +0100

    webapicommon  book osztály

---
+++ NI-csharp-project/NIcsharp/WebApi_Common/Models/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi_Common.Models
+{
+    public class Book
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "Author field is required!")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "Title field is required!")]
+        public string Title { get; set; }
+
+        [Required]
+        public bool IsBorrowed { get; set; }
+
+        [BorrowerNameValidation]
+        public string NameOfBorrower { get; set; }
+
+        public DateTime DateOfBorrowing { get; set; }
+
+        [ReturnDateValidation]
+        public DateTime DateOfReturn { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Author}: {Title}";
+        }
+
+        public Book()
+        {
+            IsBorrowed = false;
+            NameOfBorrower = "No previous borrower";
+            DateOfBorrowing = DateTime.MaxValue;
+            DateOfReturn = DateTime.MaxValue;
+        }
+    }
+}
---
+++ NI-csharp-project/NIcsharp/WebApi_Common/Models/ReturnDateValidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi_Common.Models
+{
+    public class ReturnDateValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var date = Convert.ToDateTime(value);
+
+            if (DateTime.Compare(date, DateTime.Now) < 0)
+            {
+                return new ValidationResult("The return date cannot be less then the borrowing date",
+                new[] { validationContext.MemberName });
+            }
+
+            return null;
+        }
+    }
+}
