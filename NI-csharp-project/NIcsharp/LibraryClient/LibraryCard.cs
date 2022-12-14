71d86353a2af7304c6f2bb89e83db8352966960c
Gedeon Szabo <gedeonszabo07@gmail.com>
2022-12-14 12:53:46 +0100

    Added Library Cards file, also razor file for AB

--- NI-csharp-project/NIcsharp/LibraryClient/LibraryCard.cs
+++ NI-csharp-project/NIcsharp/LibraryClient/LibraryCard.cs
@@ -6,20 +6,19 @@ using System.Net.Http.Json;
 using System.Threading.Tasks;
 using WebApi_Common.Models;
 
-namespace LibrarianApp.Pages
+namespace LibraryClient.Pages
 {
-    public partial class AvailableBooks
+    public partial class LibraryCard
     {
         [Inject]
         public HttpClient HttpClient { get; set; }
-
+        
         public Book[] Books { get; set; }
 
-        public string SearchText = "";
+        public string SearchName = "";
 
         public IEnumerable<Book> FilteredBooks =>
-            Books.Where(b => b.Title.ToLower().Contains(SearchText.ToLower()) || b.Author.ToLower().Contains(SearchText.ToLower()))
-                 .Where(b => !b.IsBorrowed);
+            Books.Where(b => b.NameOfBorrower == SearchName && b.IsBorrowed).OrderBy(b => b.DateOfReturn);
 
         protected override async Task OnInitializedAsync()
         {
