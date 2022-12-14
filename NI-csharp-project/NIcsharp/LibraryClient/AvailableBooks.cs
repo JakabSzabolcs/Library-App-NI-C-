e99f9df8ed2b2604bfa6e40e2eb0b0b013ab7905
Gedeon Szabo <gedeonszabo07@gmail.com>
2022-12-14 12:14:32 +0100

    Added AB.cs

--- NI-csharp-project/NIcsharp/LibraryClient/AvailableBooks.cs
+++ NI-csharp-project/NIcsharp/LibraryClient/AvailableBooks.cs
@@ -6,3 +6,25 @@ using System.Net.Http.Json;
 using System.Threading.Tasks;
 using WebApi_Common.Models;
 
+namespace LibrarianApp.Pages
+{
+    public partial class AvailableBooks
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        public Book[] Books { get; set; }
+
+        public string SearchText = "";
+
+        public IEnumerable<Book> FilteredBooks =>
+            Books.Where(b => b.Title.ToLower().Contains(SearchText.ToLower()) || b.Author.ToLower().Contains(SearchText.ToLower()))
+                 .Where(b => !b.IsBorrowed);
+
+        protected override async Task OnInitializedAsync()
+        {
+            Books = await HttpClient.GetFromJsonAsync<Book[]>("book");
+            await base.OnInitializedAsync();
+        }
+    }
+}
