ceee9e83b09b520e8ac4c0a9eb780ace6b48f58b
Gedeon Szabo <gedeonszabo07@gmail.com>
2022-12-22 22:14:54 +0100

    If not valid, -1.

--- NI-csharp-project/NIcsharp/WebApi_Common.Tests/ReturnDateValidationUnitTests.cs
+++ NI-csharp-project/NIcsharp/WebApi_Common.Tests/ReturnDateValidationUnitTests.cs
@@ -22,5 +22,20 @@ namespace WebApi_Common.Tests
             // Assert
             Assert.AreEqual(validationResult, ValidationResult.Success);
         }
+        
+        [TestMethod]
+        public void IsValid_WithInvalidDate_ReturnsError()
+        {
+            // Arrange
+            var date = DateTime.Now.AddDays(-1);
+            var validationContext = new ValidationContext(date);
+            var attribute = new ReturnDateValidation();
+
+            // Act
+            ValidationResult validationResult = attribute.GetValidationResult(date, validationContext);
+
+            // Assert
+            Assert.AreEqual(validationResult.ErrorMessage, "The return date cannot be less then the borrowing date");
+        }
     }
 }
