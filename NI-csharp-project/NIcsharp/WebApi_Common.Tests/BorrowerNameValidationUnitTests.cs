9636e4473cb81405e6010701b6e815323f673829
Gedeon Szabo <gedeonszabo07@gmail.com>
2022-12-22 22:10:10 +0100

    Name Validation tests.

--- NI-csharp-project/NIcsharp/WebApi_Common.Tests/BorrowerNameValidationUnitTests.cs
+++ NI-csharp-project/NIcsharp/WebApi_Common.Tests/BorrowerNameValidationUnitTests.cs
@@ -2,3 +2,26 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.ComponentModel.DataAnnotations;
 using WebApi_Common.Models;
 
+namespace WebApi_Common.Tests
+{
+[TestClass]
+    public class BorrowerNameValidationUnitTests : BorrowerNameValidation
+    {
+        [DataRow("John Doe")]
+        [DataRow("Jonh Smith")]
+        [DataRow("Joanna Roe")]
+        [DataTestMethod]
+        public void IsValid_WithValidName_ValidationSuccess(string name)
+        {
+            // Arrange
+            var validationContext = new ValidationContext(name);
+            var attribute = new BorrowerNameValidation();
+
+            // Act
+            ValidationResult validationResult = attribute.GetValidationResult(name, validationContext);
+
+            // Assert
+            Assert.AreEqual(validationResult, ValidationResult.Success);
+        }
+    }
+}
